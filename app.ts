a86a922629b39bd2613b18509dfa792fcc248648
u53f <scalewithusef@gmail.com>
2022-10-31 16:37:16 +0100

    Inferred Types

--- app.ts
+++ app.ts
@@ -7,3 +7,13 @@ const greeting = (name: string, message: string): string => {
 };
 
 console.log(greeting("u53f", msg));
+
+// * NOTE : you don't have to set types manually, Typescript will handle that
+
+// !Inferred Types on Arrays
+
+const arr = [1, 3];
+// arr.push("sdf");
+arr.push(23);
+
+const arr1: any = [1, true, "x"];
